a1d5f64b47de5564ffbd4a5816dbc8a796ad4824
Cameron Poehner <cameronpoehner@gmail.com>
2021-06-03 11:36:27 -0700

    setup Initial Page Layout.

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return(
+        <div>
+            <p>I'm a navbar</p>
+        </div>
+    )
+}
---
+++ components/Page.js
@@ -0,0 +1,10 @@
+import Header from './Header';
+
+export default function Page() {
+    return(
+        <div>
+            <Header />
+            <p>I'm a p!</p>
+        </div>
+    )
+}
