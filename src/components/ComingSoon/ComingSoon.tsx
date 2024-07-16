50d994a9bcb00476c58ae4064983f10424f19dd3
Cameron Poehner <cameronpoehner@gmail.com>
2024-07-15 19:14:30 -0700

    feat: initial setup for Coming Soon component

---
+++ src/components/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,11 @@
+import { Carousel } from '@/components/ui/carousel';
+
+const ComingSoon = async () => {
+  return (
+    <Carousel>
+      <p>Coming Soon Component</p>
+    </Carousel>
+  );
+};
+
+export default ComingSoon;
