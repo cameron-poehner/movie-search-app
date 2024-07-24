62176fd94385e7bd507ffd05bb6c660048c9e786
Cameron Poehner <cameronpoehner@gmail.com>
2024-07-23 19:04:27 -0700

    feat: updated card styles for search page results

--- src/app/search/[search]/page.tsx
+++ src/app/search/[search]/page.tsx
@@ -6,45 +6,55 @@ const Page = async ({ params }: { params: { search: string } }) => {
   const { shows, people, movies } = await GET_SEARCH_RESULTS(params.search);
 
   return (
-    <main className="h-auto min-h-screen w-full flex flex-col justify-center items-center">
-      <Tabs className="h-full flex flex-col justify-center items-center my-20">
-        <TabsList className="bg-neutral-950">
-          {movies && <TabsTrigger value="movies">movies</TabsTrigger>}
-          {shows && <TabsTrigger value="shows">shows</TabsTrigger>}
-          {people.length > 1 && (
+    <main className="min-h-screen w-full flex flex-col justify-center items-center">
+      <Tabs className="h-full bg-neutral-900 rounded-lg p-4 w-5/6 flex flex-col justify-center items-start">
+        <TabsList className="bg-neutral-950 ">
+          {movies.length > 0 && (
+            <TabsTrigger value="movies">movies</TabsTrigger>
+          )}
+          {shows.length > 0 && <TabsTrigger value="shows">shows</TabsTrigger>}
+          {people.length > 0 && (
             <TabsTrigger value="people">people</TabsTrigger>
           )}
         </TabsList>
 
-        <TabsContent value="movies" className="h-full">
-          {movies.map((movie: any) => (
-            <DetailCard
-              key={movie.id}
-              props={movie}
-              href={`/movies/${movie.id}`}
-              image={movie.poster_path}
-            />
-          ))}
+        <TabsContent value="movies" className="h-full w-full">
+          {movies.length > 0 &&
+            movies.map((movie: any) => (
+              <DetailCard
+                key={movie.id}
+                props={movie}
+                href={`/movies/${movie.id}`}
+                image={movie.poster_path}
+                popularity={movie.popularity}
+                date={movie.release_date}
+              />
+            ))}
         </TabsContent>
         <TabsContent value="shows" className="h-full w-full">
-          {shows.map((show: any) => (
-            <DetailCard
-              key={show.id}
-              props={show}
-              href={`/shows/${show.id}`}
-              image={show.poster_path}
-            />
-          ))}
+          {shows.length > 0 &&
+            shows.map((show: any) => (
+              <DetailCard
+                key={show.id}
+                props={show}
+                href={`/shows/${show.id}`}
+                image={show.poster_path}
+                popularity={show.popularity}
+                date={show.first_air_date}
+              />
+            ))}
         </TabsContent>
-        <TabsContent value="people" className="h-full w-full">
-          {people.map((person: any) => (
-            <DetailCard
-              key={person.id}
-              props={person}
-              image={person.profile_path}
-              href={`/people/${person.id}`}
-            />
-          ))}
+        <TabsContent value="people" className="h-full w-full =">
+          {people.length > 0 &&
+            people.map((person: any) => (
+              <DetailCard
+                key={person.id}
+                props={person}
+                image={person.profile_path}
+                href={`/people/${person.id}`}
+                popularity={person.popularity}
+              />
+            ))}
         </TabsContent>
       </Tabs>
     </main>
--- src/components/ui/DetailCard/DetailCard.tsx
+++ src/components/ui/DetailCard/DetailCard.tsx
@@ -14,23 +14,28 @@ interface Details {
 const DetailCard = (props: any) => {
   console.log('🚀 props', props);
   return (
-    <Card className="w-full h-52">
+    <Card className="w-full h-full mt-2 border-white">
       <Link href={props.href}>
         <CardContent className="flex">
-          <div className="relative w-20 h-28">
+          <div className="relative max-w-20 w-full h-max min-h-28">
             {props.image ? (
               <Image
                 src={`https://image.tmdb.org/t/p/w500${props.image}`}
                 alt="image title"
                 fill
+                className="rounded-lg rounded-r-none  object-fit"
               />
             ) : (
               <div>Image not Found</div>
             )}
           </div>
-          <div>
-            <CardTitle>{props.props.title || props.props.name}</CardTitle>
-            <CardDescription>{props.props.description}</CardDescription>
+          <div className="flex flex-col flex-wrap items-start ml-2">
+            <CardTitle className="text-xs">
+              {props.props.title || props.props.name}
+            </CardTitle>
+            <CardDescription>
+              {props.props.release_date || props.props.first_air_date}
+            </CardDescription>
           </div>
         </CardContent>
       </Link>
